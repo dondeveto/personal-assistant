65366a41c4afed2a90809bf4bc9252cf4ea176ef
dondeveto <d3vindobson@gmail.com>
2018-07-08 20:16:11 -0500

    hello i made some changes to the weather function i actually recoded it and added speech to it

--- apis.py
+++ apis.py
@@ -1,22 +1,42 @@
-import requests
-import json 
-import sys
+import re
+import urllib.request
+import time
+import pyttsx
 
-def fetch_weather(city):
-    try:
-        u = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22{0}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
-        r = requests.get(u.format(city))
-        r = requests.get(u.format("North Pole"))
-        j = json.loads(r.text)
-        results = j['query']['results']['channel']['item']
-        return results
-    except Exception as e:
-        print(e)
-        sys.stderr.write("Couldn't load current conditions\n")
-        return "Unkonw" # typo?
+location = "" 
+
+def fetch_weather(location):
+    location=location.capitalize()
+    if location == "":
+        exit
+    else:
+        try:   
+            city = location.replace(" ", "-")
+            url = "http://www.weather-forecast.com/locations/" + city + "/forecasts/latest"
+            
+            data = urllib.request.urlopen(url).read()
+            data1 = data.decode("utf-8")
+            m = re.search('span class="phrase">', data1)
+            start = m.end()
+            end = start + 300
+            newString = data1[start:end]
+
+            
+
+            m = re.search("</span>", newString)
+            end = m.start() - 2
+            final = newString[0:end]
+            
+            engine = pyttsx.init()
+            engine.say(final)
+            engine.runAndWait()
+            return final
+            fetch_weather()
+        except:
+            return "The city doesn't exist"
+            #time.sleep(2)
+            fetch_weather()
 
 
-def fetch_stocks(ticker):
-    return "1 gazillon dollars" # TODO? 
 
-# TODO: More integrations
+
